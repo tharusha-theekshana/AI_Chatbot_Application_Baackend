8d34189526dad544669bab9f2715134b2b0f3a1c
Theekshana <theenutheekshana@gmail.com>
2025-04-05 13:48:13 +0530

    Develop chat post request to get response from gemini API.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from app.config import settings
+from app.model import ChatRequest, ChatResponse
+
+import google.generativeai as generativeAi
 
 app = FastAPI()
 
@@ -7,7 +10,17 @@ app = FastAPI()
 apiKey = settings.GOOGLE_API_KEY
 gemini_version = settings.GEMINI_VERSION
 
+# Config google ai client
+generativeAi.configure(api_key=apiKey)
+model = generativeAi.GenerativeModel(gemini_version)
 
-@app.get("/")
-def testRequest():
-    return {"message : Test Request"}
+
+# Post endpoint
+@app.post("/chat", response_model=ChatResponse)
+async def chat(request: ChatRequest):
+    try:
+        response = model.generate_content(request.message)
+        response_text = response.text
+        return ChatResponse(reply=response_text)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error with response generate : {str(e)}")
