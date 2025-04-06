2e5f6c3ad428e2410821b8a12fce777f187a0e55
Theekshana <theenutheekshana@gmail.com>
2025-04-06 13:36:17 +0530

    Add CORS settings.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.config import settings
 from app.model import ChatRequest, ChatResponse
 
@@ -12,6 +14,15 @@ apiKey = settings.GOOGLE_API_KEY
 # Config google ai client
 generativeAi.configure(api_key=apiKey)
 model = generativeAi.GenerativeModel("gemini-2.0-flash")
+
+# CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Post endpoint
